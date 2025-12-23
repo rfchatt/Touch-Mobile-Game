de4ca73e58a6c7e0dd3bbab2a8f452528e029d9a
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-12-23 20:45:36 +0100

    Update MainViewModel.kt

--- app/src/main/java/com/example/scoreboard/MainViewModel.kt
+++ app/src/main/java/com/example/scoreboard/MainViewModel.kt
@@ -8,7 +8,7 @@ class MainViewModel : ViewModel() {
     val score = MutableLiveData<Int>(0)
 
     fun addOne() {
-
+        // Récupération de valeur initial + l'Ajoute de 1
         val actualValue = score.value ?: 0
         score.value = actualValue + 1
 
