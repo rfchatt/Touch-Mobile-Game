fedaa8c5fd13b6ca5dc9ba1c3ba2ec17662702cb
Abderrafie Chate | الشاط عبدالرفيع <chattabdrrafie@gmail.com>
2025-12-23 20:45:13 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/scoreboard/MainActivity.kt
+++ app/src/main/java/com/example/scoreboard/MainActivity.kt
@@ -16,15 +16,18 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // L'initialisation du ViewModel
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         val textScore = findViewById<TextView>(R.id.text_counter)
         val addButton = findViewById<Button>(R.id.click_button)
 
+        // Observation : Si le score change dans le ViewModel, on met a jour le texte
         viewModel.score.observe(this) { newScore ->
             textScore.text = newScore.toString()
         }
 
+        // L'action de Click --> on demande le ViewModel de faire le travail
         addButton.setOnClickListener {
             viewModel.addOne()
         }
